01cb95499162a74479f4294226cc97c45e816a48
kmc369 <keishacoleman710@gmail.com>
2023-10-17 20:44:28 -0400

    Songs route started

--- app/api/song_routes.py
+++ app/api/song_routes.py
@@ -18,7 +18,7 @@ def get_songs_of_album(albumid):
 
 
 @songs_bp.route("/user/<int:userid>", methods=["GET"])
-def get_songs_of_album(userid):
+def get_user_songs(userid):
     """GET ALL THE SONGS OF THE USER hello""" 
 
     songs = Song.query.filter_by(user_id=userid).all()
