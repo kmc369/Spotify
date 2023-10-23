389410dd6faa99c041460dfab35648a1350d81d6
kmc369 <keishacoleman710@gmail.com>
2023-10-23 14:42:04 -0400

    cascade

--- app/models/albums.py
+++ app/models/albums.py
@@ -16,7 +16,7 @@ class Album(db.Model):
     user_id =  db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod("users.id")),nullable=False)
     # song_id = db.Column(db.Integer,  db.ForeignKey(add_prefix_for_prod("songs.id")),nullable=False)
  #relationships
-    songs =  db.relationship("Song", back_populates="albums")
+    songs =  db.relationship("Song", back_populates="albums",cascade="all, delete-orphan")
     artists = db.relationship("Artist", back_populates="albums")
     user = db.relationship("User", back_populates="albums")
     
