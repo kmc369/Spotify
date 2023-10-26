6188950ac3fff35bdc5c9edc4720c8881ad2b081
kmc369 <keishacoleman710@gmail.com>
2023-10-26 06:12:11 -0400

    Playlist backend done

--- app/api/playlist_routes.py
+++ app/api/playlist_routes.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, jsonify, request 
 from flask_login import login_required
-from app.models import db,Playlist
+from app.models import db,Playlist,Song
 from .aws_helper import get_unique_filename,upload_file_to_s3,remove_file_from_s3
 from ..forms.playlist_form import PlaylistForm
 playlist_bp = Blueprint('playlist',__name__)
@@ -11,7 +11,6 @@ def create_playlist():
     form = PlaylistForm()
     
     form['csrf_token'].data = request.cookies['csrf_token']
-    print("the form data is",form.data)
     if form.validate_on_submit():
         image = form.data.get("image")
         if image:
@@ -32,3 +31,17 @@ def create_playlist():
         db.session.commit()
         return jsonify(new_playlist.to_dict())
     return jsonify({"error": form.errors}), 400
+
+
+@playlist_bp.route("/<int:playlistId>", methods=["GET"])
+def get_songs_on_playlist(playlistId):
+    """get playlist of songs"""
+    songs = Song.query.filter_by(playlist_id=playlistId)
+    if not songs:
+        return jsonify({"message":"No song found"},400) 
+    
+    
+    return [song.to_dict() for song in songs]
+    
+    
+    
